0c33e19aacb75256e3ccdba71ce310a17d6efec7
Juan Carlos Chipana Alvarado <jcalvarado347@gmail.com>
2024-05-20 09:46:40 -0400

    se creo en nestjs

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller({})
+export class TaskController{
+
+ @Get('/tasks')
+  getAllTasks(){
+    return 'Obteniendo todas las tareas';
+  }
+
+
+}
---
+++ src/tasks/tasks.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TaskController } from "./tasks.controller";
+
+@Module({
+  controllers:[TaskController],
+})
+export class TasksModule {}
+
+
+
